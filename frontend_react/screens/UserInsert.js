9ed10eea993693aa88ed8ba240f5ea727b9d8bf7
ioedumac <eacevedof@yahoo.es>
2020-12-20 16:21:44 +0100

    formateado el form insert

--- frontend_react/screens/UserInsert.js
+++ frontend_react/screens/UserInsert.js
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, Button, TextInput, ScrollView, View } from 'react-native'
 
 const UserInsert = ()=>{
+
+  const [state, set_state] = useState({
+    name: '', email: '', phone: ''
+  })
+
+
+  const handleChangeText = (name, value) => {
+    set_state({...state, [name]: value})
+  }
+
   return (
     <ScrollView style={styles.container} >
       <View style={styles.inputgroup} >
-        <TextInput placeholder="Name" />
+        <TextInput placeholder="Name" onChangeText={v => handleChangeText('name', v)} />
       </View>
       <View style={styles.inputgroup}>
-        <TextInput placeholder="email" />
+        <TextInput placeholder="email" onChangeText={v => handleChangeText('email', v)} />
       </View>
       <View style={styles.inputgroup}>
-        <TextInput placeholder="phone" />
+        <TextInput placeholder="phone" onChangeText={v => handleChangeText('phone', v)}/>
       </View>
       <View style={styles.inputgroup}>
-        <Button title="Save User" />
-      </View>      
+        <Button title="Save User" onPress={()=>console.log(state)} />
+      </View>
     </ScrollView>
   )
 }
@@ -23,7 +33,7 @@ const UserInsert = ()=>{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 35,
+    padding: 40,
   },
 
   inputgroup: {

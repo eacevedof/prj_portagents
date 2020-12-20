a05c14b961c51d56adf27535e02db6615e164757
ioedumac <eacevedof@yahoo.es>
2020-12-20 15:49:16 +0100

    form insert ok

--- frontend_react/screens/UserInsert.js
+++ frontend_react/screens/UserInsert.js
@@ -3,21 +3,37 @@ import { StyleSheet, Button, TextInput, ScrollView, View } from 'react-native'
 
 const UserInsert = ()=>{
   return (
-    <ScrollView>
-      <View>
+    <ScrollView style={styles.container} >
+      <View style={styles.inputgroup} >
         <TextInput placeholder="Name" />
       </View>
-      <View>
+      <View style={styles.inputgroup}>
         <TextInput placeholder="email" />
       </View>
-      <View>
+      <View style={styles.inputgroup}>
         <TextInput placeholder="password" />
       </View>
-      <View>
+      <View style={styles.inputgroup}>
         <Button title="Save User" />
       </View>      
     </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35,
+  },
+
+  inputgroup: {
+    flex: 1,
+    padding:0,
+    marginBottom:15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
+})
+
 
 export default UserInsert

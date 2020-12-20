9c3b3d5118881f126955c41cce150d1fd18f1edc
ioedumac <eacevedof@yahoo.es>
2020-12-20 15:20:21 +0100

    instalo y configuro naviation/stack

--- frontend_react/App.js
+++ frontend_react/App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+function get_stack(){
+  
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Portagents Voucher</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +24,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
